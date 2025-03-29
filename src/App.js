76c27f616d7d766d6d0bf4e22d73d243cddd0381
Gabriel Vieira <vieira.gabriel7901@gmail.com>
2025-03-29 13:25:16 -0300

    Criando meus primeiros componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld'
+
 
 function App() {
+  const name = 'Gabriel';
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <p>Olá, {name}</p>
+      <HelloWorld />
     </div>
   );
 }
