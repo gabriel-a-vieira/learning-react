28d03057215a281e2e17e278eea8c4bfc229dc77
Gabriel Vieira <vieira.gabriel7901@gmail.com>
2025-03-31 22:54:17 -0300

    Inserindo Props + Componente Pessoa

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld'
-
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
-  const name = 'Gabriel';
+
+  const nome = "Camile"
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <p>Olá, {name}</p>
-      <HelloWorld />
+      <SayMyName nome="Gabriel" />
+      <SayMyName nome="Geraldo" />
+      <SayMyName nome={nome} />
+      <Pessoa 
+        nome="Gabriel Vieira" 
+        idade="23" 
+        profissao="Programador" 
+        foto="https://img.freepik.com/vetores-gratis/empresario-andando-com-confianca-segurando-o-icone-da-maleta-isolado_24911-109659.jpg" 
+      />
     </div>
   );
+
 }
 
 export default App;
