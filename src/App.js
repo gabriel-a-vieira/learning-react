aa5bae446682282e2f8b34e08656b6a96db8e5be
Gabriel Vieira <vieira.gabriel7901@gmail.com>
2025-04-21 12:38:49 -0300

    Aula 8 e 9 - PropTypes, DefaultProps, Distructuring e Eventos no React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import './App.css';
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
-import Frase from './components/Frase'
-import List from './components/List'
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
 
-  const nome = "Camile"
-
   return (
     <div className="App">
-      <Frase />
-      <SayMyName nome="Gabriel" />
-      <SayMyName nome="Geraldo" />
-      <SayMyName nome={nome} />
-      <Pessoa 
-        nome="Gabriel Vieira" 
-        idade="23" 
-        profissao="Programador" 
-        foto="https://img.freepik.com/vetores-gratis/empresario-andando-com-confianca-segurando-o-icone-da-maleta-isolado_24911-109659.jpg" 
-      />
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2"/>
+      <Form />
     </div>
   );
 
--- src/components/List.js
+++ src/components/List.js
@@ -5,9 +5,10 @@ function List() {
         <>
             <h1>Lista</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="Cherry"/>
-                <Item marca="Porsche"/>
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="Cherry" ano_lancamento={1986}/>
+                <Item marca="Porsche" ano_lancamento={1987}/>
+                <Item marca={12} ano_lancamento={'1029'}/>
             </ul>
         </>
     )
