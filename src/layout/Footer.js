17a173f105e89f31dd0fec53e551d19a9fefa3f4
Gabriel Vieira <vieira.gabriel7901@gmail.com>
2025-06-08 17:59:08 -0300

    Aula 15 - React Icons

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,7 +1,17 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+
+import styles from './Footer.module.css';
+
 function Footer() {
 
     return (
-        <p>Rodapé</p>
+        <footer>
+            <ul className={styles.social_list}>
+                <li><FaFacebook/></li>
+                <li><FaLinkedin/></li>
+                <li><FaInstagram/></li>
+            </ul>
+        </footer>
     )
     
 }
